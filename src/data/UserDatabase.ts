3138852f680e3870a189ec0ea8d10d6e785ffc3a
Géssica Costa de Araújo Souza <gcas_@hotmail.com>
2020-07-15 15:32:41 -0300

    Inicio do projeto.

---
+++ src/data/UserDatabase.ts
@@ -0,0 +1,59 @@
+import knex from "knex";
+import { BaseDatabase } from "./BaseDatabase";
+import { USER_ROLES } from '../services/Authenticator'
+
+export class UserDatabase extends BaseDatabase {
+
+  private static TABLE_NAME = "UserCook";
+
+  public async createUser(
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    role: USER_ROLES
+  ): Promise<void> {
+    await this.getConnection()
+      .insert({
+        id,
+        name,
+        email,
+        password,
+        role
+      })
+      .into(UserDatabase.TABLE_NAME);
+
+    BaseDatabase.destroyConnection()
+  }
+
+  public async getUserByEmail(email: string): Promise<any> {
+    const result = await this.getConnection()
+      .select("*")
+      .from(UserDatabase.TABLE_NAME)
+      .where({ email });
+
+    BaseDatabase.destroyConnection()
+
+    return result[0];
+  }
+
+  public async getUserById(id: string): Promise<any> {
+    const result = await this.getConnection()
+      .select("*")
+      .from(UserDatabase.TABLE_NAME)
+      .where({ id });
+
+    BaseDatabase.destroyConnection()
+
+    return result[0];
+  }
+
+  public async deleteUser(id: string): Promise<void> {
+    await this.getConnection().raw(`
+    DELETE FROM ${UserDatabase.TABLE_NAME}
+    WHERE id = "${id}"
+    `)
+
+    BaseDatabase.destroyConnection()
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,167 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import { AddressInfo } from "net";
+import { IdGenerator } from "./services/IdGenerator";
+import { UserDatabase } from "./data/UserDatabase";
+import { Authenticator } from "./services/Authenticator";
+import { HashManager } from "./services/HashManager";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/signup", async (req: Request, res: Response) => {
+  try {
+    
+    if (!req.body.email || req.body.email.indexOf("@") === -1) {
+      throw new Error("Invalid email");
+    }
+
+    if (!req.body.password || req.body.password.length < 6) {
+      throw new Error("Invalid password");
+    }
+
+    const userData = {
+      email: req.body.email,
+      name: req.body.name,
+      password: req.body.password,
+      role: req.body.role
+    };
+
+    const hashManager = new HashManager()
+    const cipherText = await hashManager.hash(userData.password)
+
+    const idGenerator = new IdGenerator();
+    const id = idGenerator.generate();
+
+    const userDb = new UserDatabase();
+    await userDb.createUser(id, userData.name, userData.email, cipherText, userData.role);
+
+    const authenticator = new Authenticator();
+    const token = authenticator.generateToken({
+      id, role: userData.role
+    });
+
+    res.status(200).send({
+      token,
+    });
+  } catch (err) {
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
+
+app.post("/login", async (req: Request, res: Response) => {
+  try {
+    
+    if (!req.body.email || req.body.email.indexOf("@") === -1) {
+      throw new Error("Invalid email");
+    }
+
+    const userData = {
+      email: req.body.email,
+      password: req.body.password,
+    };
+
+    const userDatabase = new UserDatabase();
+    const user = await userDatabase.getUserByEmail(userData.email);
+
+    const hashManager = new HashManager()
+    const passwordIsCorrect = await hashManager.compare(userData.password, user.password)
+
+    if (!passwordIsCorrect) {
+      throw new Error("Invalid password");
+    }
+
+    const authenticator = new Authenticator();
+    const token = authenticator.generateToken({
+      id: user.id,
+      role: user.role
+    });
+
+    res.status(200).send({
+      token,
+    });
+  } catch (err) {
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
+
+app.delete("/user/:id", async (req: Request, res: Response) => {
+  try {
+    const authenticator = new Authenticator()
+    const tokenData = authenticator.getData(req.headers.authorization as string)
+
+    if (tokenData.role !== "ADMIN") {
+      throw new Error("Apenas administradores podem deletar outro usuário")
+    }
+
+    const userDB = new UserDatabase()
+    await userDB.deleteUser(req.params.id)
+
+    res.status(200).send({
+      message: "Usuário deletado"
+    })
+  } catch (err) {
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
+
+app.get("/user/:id", async (req: Request, res: Response) => {
+  try {
+    const authenticator = new Authenticator();
+    const tokenData = authenticator.getData(req.headers.authorization as string);
+
+    const userDb = new UserDatabase();
+    const user = await userDb.getUserById(req.params.id);
+
+    res.status(200).send({
+      id: user.id,
+      email: user.email
+    });
+  } catch (err) {
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
+
+app.get("/user/profile", async (req: Request, res: Response) => {
+  try {
+    const authenticator = new Authenticator();
+    const tokenData = authenticator.getData(req.headers.authorization as string);
+
+    if(tokenData.role !== "NORMAL") {
+      throw new Error("Unauthorized")
+    }
+
+    const userDb = new UserDatabase();
+    const user = await userDb.getUserById(tokenData.id);
+
+    res.status(200).send({
+      id: user.id,
+      email: user.email,
+      role: user.role
+    });
+  } catch (err) {
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+});
+
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server is running in http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure upon starting server.`);
+  }
+});
